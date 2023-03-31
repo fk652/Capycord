954464f4939511c26b7991c970d2da5bf65fec28
Fahim Khan <fk652@nyu.edu>
2023-03-31 16:53:43 -0400

    finish User model validations

---
+++ app/models/user.rb
@@ -0,0 +1,103 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :bigint           not null, primary key
+#  email               :string           not null
+#  username            :string           not null
+#  password_digest     :string           not null
+#  session_token       :string           not null
+#  online_status       :string           default("offline"), not null
+#  set_online_status   :string
+#  custom_status       :string           default(""), not null
+#  profile_picture_url :string           default(""), not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+class User < ApplicationRecord
+  has_secure_password
+
+  STATUS = [
+    "online",
+    "offline",
+    "idle",
+    "busy",
+    "invisible"
+  ]
+
+  validates :email, :session_token, 
+    presence: true, 
+    uniqueness: true
+  validates :username,
+    presence: true
+  validates :username, 
+    length: { in: 2..32 }, 
+    format: { without: URI::MailTo::EMAIL_REGEXP, message: "can't be email" }
+  validates :username,
+    format: { without: /@|#|:|```|discord/, message: "can't include @, #, :, ```, discord"}
+  validates :username,
+    format: { without: /(^everyone$)|(^here$)/, message: "can't be 'everyone' or 'here'"}
+  validates :email, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, 
+    length: { in: 8..128 }, 
+    allow_nil: true
+  validates :custom_status,
+    length: { maximum: 128 }
+
+  before_validation :ensure_session_token
+  before_create :add_tag_number
+  before_update :ensure_unique_tag_username
+
+  after_validation :custom_status, :online_status,
+    presence: true
+  after_validation :online_status, :set_online_status,
+    inclusion: { in: STATUS, message: "'%{value}' is not a valid status"}
+
+  def self.find_by_credentials(credential, password)
+    user = URI::MailTo::EMAIL_REGEXP.match(credential) ? User.find_by(email: credential) : User.find_by(username: credential)
+    return user && user.authenticate(password) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token()
+    self.save!
+    return self.session_token
+  end
+
+  private
+  def generate_unique_session_token
+    while true
+      token = SecureRandom.urlsafe_base64
+      return token unless User.exists?(session_token: token)
+    end
+  end
+
+  def generate_random_tag_number
+    "##{rand(0..9)}#{rand(0..9)}#{rand(0..9)}#{rand(0..9)}"
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token()
+  end
+
+  def add_tag_number
+    while true
+      tag_number = generate_random_tag_number
+      unless User.exists?("#{self.username}#{tag_number}")
+        self.username = "#{self.username}#{tag_number}"
+        return
+      end
+    end
+  end
+
+  def ensure_unique_tag_username
+    if self.username_changed?
+      while User.exists?(self.username)
+        tag = generate_random_tag_number
+        self.username = self.username.split("#")[0] + tag
+      end
+    end
+  end
+end
---
+++ db/migrate/20230331175022_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :online_status, null: false, default: "offline"
+      t.string :set_online_status
+      t.string :custom_status, null: false, default: ""
+      t.string :profile_picture_url, null: false, default: ""
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
