d2ca6935943ba70e9d11fa9e78863297c8e7db46
Fahim Khan <fk652@nyu.edu>
2023-04-01 18:26:01 -0400

    setup frontend auth, login, signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,29 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormPage";
+import LoginFormPage from "./components/LoginFormPage";
+import HomePage from "./components/HomePage";
+import SplashPage from "./components/SplashPage";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <Switch>
+        <Route path="/signup">
+          <SignupFormPage />
+        </Route>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route exact path="/">
+          {/* <SplashPage /> */}
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
