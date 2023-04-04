8373ae53a0febf5e1969d1af9199b8d910b35fcc
Fahim Khan <fk652@nyu.edu>
2023-04-04 13:01:50 -0400

    add server icon css, update css

--- frontend/src/components/FriendsDisplay/index.js
+++ frontend/src/components/FriendsDisplay/index.js
@@ -45,11 +45,11 @@ const FriendsDisplay = () => {
     <div className="friends-display">
       <div className="friends-nav-bar">
         <div className="friends-options">
-          <div className="icon-wrapper">
+          <div className="icon-wrapper no-hover">
             {/* <img className="friends-nav-icon" src={friendIcon} alt="friend-icon"/> */}
             <svg className="friends-nav-icon" x="0" y="0" aria-hidden="true" role="img" width="24" height="24" viewBox="0 0 24 24">
-              <g fill="none" fill-rule="evenodd">
-                <path fill="currentColor" fill-rule="nonzero" d="M0.5,0 L0.5,1.5 C0.5,5.65 2.71,9.28 6,11.3 L6,16 L21,16 L21,14 C21,11.34 15.67,10 13,10 C13,10 12.83,10 12.75,10 C8,10 4,6 4,1.5 L4,0 L0.5,0 Z M13,0 C10.790861,0 9,1.790861 9,4 C9,6.209139 10.790861,8 13,8 C15.209139,8 17,6.209139 17,4 C17,1.790861 15.209139,0 13,0 Z" transform="translate(2 4)">
+              <g fill="none" fillRule="evenodd">
+                <path fill="currentColor" fillRule="nonzero" d="M0.5,0 L0.5,1.5 C0.5,5.65 2.71,9.28 6,11.3 L6,16 L21,16 L21,14 C21,11.34 15.67,10 13,10 C13,10 12.83,10 12.75,10 C8,10 4,6 4,1.5 L4,0 L0.5,0 Z M13,0 C10.790861,0 9,1.790861 9,4 C9,6.209139 10.790861,8 13,8 C15.209139,8 17,6.209139 17,4 C17,1.790861 15.209139,0 13,0 Z" transform="translate(2 4)">
                 </path>
                 <path d="M0,0 L24,0 L24,24 L0,24 L0,0 Z M0,0 L24,0 L24,24 L0,24 L0,0 Z M0,0 L24,0 L24,24 L0,24 L0,0 Z">
                 </path>
@@ -96,7 +96,7 @@ const FriendsDisplay = () => {
             {/* <img className="message-icon" src={messageIcon} alt="message-icon"/> */}
             {/* <i class="fa fa-inbox"></i> */}
             <svg x="0" y="0" width="24" height="24" viewBox="0 0 24 24">
-              <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M20.998 0V3H23.998V5H20.998V8H18.998V5H15.998V3H18.998V0H20.998ZM2.99805 20V24L8.33205 20H14.998C16.102 20 16.998 19.103 16.998 18V9C16.998 7.896 16.102 7 14.998 7H1.99805C0.894047 7 -0.00195312 7.896 -0.00195312 9V18C-0.00195312 19.103 0.894047 20 1.99805 20H2.99805Z">
+              <path fill="currentColor" fillRule="evenodd" clipRule="evenodd" d="M20.998 0V3H23.998V5H20.998V8H18.998V5H15.998V3H18.998V0H20.998ZM2.99805 20V24L8.33205 20H14.998C16.102 20 16.998 19.103 16.998 18V9C16.998 7.896 16.102 7 14.998 7H1.99805C0.894047 7 -0.00195312 7.896 -0.00195312 9V18C-0.00195312 19.103 0.894047 20 1.99805 20H2.99805Z">
               </path>
             </svg>
           </div>
--- frontend/src/store/ui.js
+++ frontend/src/store/ui.js
@@ -1,9 +1,15 @@
-const SET_FRIEND_NAV_TAB = "ui/setNavTab";
+const SET_FRIEND_NAV_TAB = "ui/setFriendNavTab";
+const SET_SELECTED_SERVER = "ui/setSelectedServer";
 const RESET_UI = 'ui/reset';
 
 export const setFriendNav = (selectedTab) => ({
   type: SET_FRIEND_NAV_TAB,
   selectedTab
+})
+
+export const setSelectedServer = (selectedServer) => ({
+  type: SET_SELECTED_SERVER,
+  selectedServer
 })
 
 export const resetUi = () => ({
@@ -14,14 +20,21 @@ export const getSelectedFriendNavTab = (state) => {
   return state.ui.selectedFriendNavTab;
 }
 
+export const getSelectedServer = (state) => {
+  return state.ui.selectedServer;
+}
+
 const initialState = {
-  selectedFriendNavTab: "friends-online"
+  selectedFriendNavTab: "friends-online",
+  selectedServer: "hom" // home
 }
 
 const uiReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_FRIEND_NAV_TAB:
       return {...state, selectedFriendNavTab: action.selectedTab}
+    case SET_SELECTED_SERVER:
+      return {...state, selectedServer: action.selectedServer}
     case RESET_UI:
       return {...initialState}
     default:
