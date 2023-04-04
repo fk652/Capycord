58015932ec2a6dbdba1b210a2e3df398eac46d5f
Fahim Khan <fk652@nyu.edu>
2023-04-04 00:25:10 -0400

    add friend display scrolling and css update

--- frontend/src/components/FriendsDisplay/FriendsAdd/index.js
+++ frontend/src/components/FriendsDisplay/FriendsAdd/index.js
@@ -7,7 +7,7 @@ const FriendsAdd = () => {
       <br />
       <iframe 
         title="lul"
-        width="100%" height="700"
+        width="100%" height="500"
         src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0&modestbranding=1" 
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
