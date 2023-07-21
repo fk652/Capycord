f47f798946711fba2c6b0f7778998312b483e7f9
Fahim Khan <fk652@nyu.edu>
2023-07-21 15:01:22 -0400

    add portfolio site to about me

--- frontend/src/components/AboutMe/index.js
+++ frontend/src/components/AboutMe/index.js
@@ -11,17 +11,20 @@ const AboutMe = () => {
         <div className="about-me-info">
           <h2 className="project-title">Capycord</h2>
           <h3 className="dev-name">By Fahim Khan</h3>
+          <div className="nav-links">
+            <a href="https://github.com/fk652/Capycord" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/fk652/" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://fahim-khan.com/" target="_blank" rel="noopener noreferrer">
+              <i className="fa fa-globe"></i>
+            </a>
+          </div>
         </div>
       </div>
 
-      <div className="nav-links">
-        <a href="https://github.com/fk652/Capycord" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/fk652/" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-linkedin"></i>
-        </a>
-      </div>
     </div>
   )
 }
